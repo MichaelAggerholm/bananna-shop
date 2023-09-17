4d25322a7a8a2aa848c0b2c4befa5a4b40f4730d
Michael Aggerholm <92aggerholm@gmail.com>
2023-09-17 07:20:43 +0200

    Tilføjet mappestruktur med start view filer og komponenter.

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+@section('name', 'Bananna Store')
+@section('content')
+    <main class="homepage">
+
+        @include('pages.components.home.header')
+
+        <h1>Forside "Home page"</h1>
+
+    </main>
+@endsection
