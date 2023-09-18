a8c483ad7affa717391f3e7616b2f1bf07600606
Michael Aggerholm <92aggerholm@gmail.com>
2023-09-18 07:18:43 +0200

    tilføjet link storage og clear cache routes for nem adgang uden terminal

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PagesController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PagesController::class, 'home'])->name('home');
+
+// Route for clearing caches
+Route::get('/clear-caches', function () {
+    Artisan::call('route:cache');
+    Artisan::call('config:cache');
+    Artisan::call('cache:clear');
+    Artisan::call('view:clear');
+    return 'clear';
+});
+
+// Route for linking storage
+Route::get('/linkstorage', function () {
+    Artisan::call('storage:link');
+});
