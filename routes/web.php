ae17831ea733e85b005190e51dec7e330272fa14
Michael Aggerholm <92aggerholm@gmail.com>
2023-09-27 21:46:18 +0200

    prepared varieties

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PagesController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\VarietyController;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
@@ -22,7 +23,7 @@ Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middl
 Route::group(['prefix' => '/adminpanel', 'middleware' => 'admin'], function () {
     Route::get('/', [AdminController::class, 'dashboard'])->name('adminpanel');
 
-    // Products routes
+    // Users routes
     Route::group(['prefix' => 'users'], function() {
         Route::get('/', [UserController::class, 'index'])->name('adminpanel.users');
         Route::get('/create', [UserController::class, 'create'])->name('adminpanel.users.create');
@@ -30,6 +31,16 @@ Route::group(['prefix' => '/adminpanel', 'middleware' => 'admin'], function () {
         Route::get('/{id}', [UserController::class, 'edit'])->name('adminpanel.users.edit');
         Route::put('/{id}', [UserController::class, 'update'])->name('adminpanel.users.edit');
         Route::delete('/{id}', [UserController::class, 'destroy'])->name('adminpanel.users.destroy');
+    });
+
+    // Varieties routes
+    Route::group(['prefix' => 'varieties'], function() {
+        Route::get('/', [VarietyController::class, 'index'])->name('adminpanel.varieties');
+        Route::get('/create', [VarietyController::class, 'create'])->name('adminpanel.varieties.create');
+        Route::post('/create', [VarietyController::class, 'store'])->name('adminpanel.varieties.store');
+        Route::get('/{id}', [VarietyController::class, 'edit'])->name('adminpanel.varieties.edit');
+        Route::put('/{id}', [VarietyController::class, 'update'])->name('adminpanel.varieties.edit');
+        Route::delete('/{id}', [VarietyController::class, 'destroy'])->name('adminpanel.varieties.destroy');
     });
 });
 
